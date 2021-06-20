660d2cc45ec87da4f2e452a58b3e42306b9c6b1b
Ruben Marquez <rubenmfdeveloper@gmail.com>
2021-06-20 12:36:09 +0200

    NEW: Start project
    #1

---
+++ marvel-app-swiftui/marvel-app-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  marvel-app-swiftui
+//
+//  Created by Ruben Marquez on 20/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ marvel-app-swiftui/marvel-app-swiftui/marvel_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_app_swiftuiApp.swift
+//  marvel-app-swiftui
+//
+//  Created by Ruben Marquez on 20/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct marvel_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
